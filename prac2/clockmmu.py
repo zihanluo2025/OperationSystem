b4f487214c5fafcf9b8e842c1434d4e0790b4bd2
zihan <zihan.luo2025@outlook.com>
2025-09-12 12:07:48 +0930

    update

--- prac2/clockmmu.py
+++ prac2/clockmmu.py
@@ -3,33 +3,122 @@ from mmu import MMU
 
 class ClockMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for EscMMU
-        pass
+        self.frames = frames  # Number of page frames
+        self.debug = False  # Debug mode flag
+        self.disk_reads = 0  # Counter for disk reads
+        self.disk_writes = 0  # Counter for disk writes
+        self.page_faults = 0  # Counter for page faults
+        
+        # Data structures for Clock algorithm
+        self.page_table = {}  # Maps page numbers to frame info
+        self.frames_list = [None] * frames  # List of frames with page info
+        self.reference_bits = [0] * frames  # Reference bits for each frame
+        self.clock_hand = 0  # Pointer for Clock algorithm
+        
+        # For each frame, we'll store (page_number, dirty_bit)
+        # Initially all frames are empty
 
     def set_debug(self):
-        # TODO: Implement the method to set debug mode
-        pass
+        self.debug = True
 
     def reset_debug(self):
-        # TODO: Implement the method to reset debug mode
-        pass
+        self.debug = False
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        pass
+        # Check if page is already in memory
+        if page_number in self.page_table:
+            frame_index = self.page_table[page_number]
+            # Set reference bit to 1
+            self.reference_bits[frame_index] = 1
+            if self.debug:
+                print(f"Page {page_number} is already in frame {frame_index}")
+        else:
+            # Page fault - need to load page
+            self.page_faults += 1
+            self.disk_reads += 1
+            
+            # Find a frame to replace using Clock algorithm
+            frame_index = self.find_victim_frame()
+            
+            # If the victim frame has a page that is dirty, write it to disk
+            if self.frames_list[frame_index] is not None:
+                victim_page, is_dirty = self.frames_list[frame_index]
+                if is_dirty:
+                    self.disk_writes += 1
+                    if self.debug:
+                        print(f"Writing page {victim_page} to disk")
+                # Remove the victim page from page table
+                if victim_page in self.page_table:
+                    del self.page_table[victim_page]
+            
+            # Load the new page
+            self.frames_list[frame_index] = (page_number, False)  # Not dirty initially
+            self.page_table[page_number] = frame_index
+            self.reference_bits[frame_index] = 1  # Set reference bit
+            
+            if self.debug:
+                print(f"Page fault: loaded page {page_number} into frame {frame_index}")
 
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        pass
+        # Check if page is already in memory
+        if page_number in self.page_table:
+            frame_index = self.page_table[page_number]
+            # Set reference bit to 1 and mark as dirty
+            self.reference_bits[frame_index] = 1
+            # Update the frame to mark it as dirty
+            self.frames_list[frame_index] = (page_number, True)
+            if self.debug:
+                print(f"Write to page {page_number} in frame {frame_index}")
+        else:
+            # Page fault - need to load page
+            self.page_faults += 1
+            self.disk_reads += 1
+            
+            # Find a frame to replace using Clock algorithm
+            frame_index = self.find_victim_frame()
+            
+            # If the victim frame has a page that is dirty, write it to disk
+            if self.frames_list[frame_index] is not None:
+                victim_page, is_dirty = self.frames_list[frame_index]
+                if is_dirty:
+                    self.disk_writes += 1
+                    if self.debug:
+                        print(f"Writing page {victim_page} to disk")
+                # Remove the victim page from page table
+                if victim_page in self.page_table:
+                    del self.page_table[victim_page]
+            
+            # Load the new page and mark it as dirty
+            self.frames_list[frame_index] = (page_number, True)
+            self.page_table[page_number] = frame_index
+            self.reference_bits[frame_index] = 1  # Set reference bit
+            
+            if self.debug:
+                print(f"Page fault: loaded page {page_number} into frame {frame_index} (dirty)")
+                
+    def find_victim_frame(self):
+        # Implement Clock algorithm to find a victim frame
+        while True:
+            # Check the current frame
+            if self.reference_bits[self.clock_hand] == 0:
+                # Found a frame with reference bit 0
+                victim_frame = self.clock_hand
+                self.clock_hand = (self.clock_hand + 1) % self.frames
+                return victim_frame
+            else:
+                # Set reference bit to 0 and move to next frame
+                self.reference_bits[self.clock_hand] = 0
+                self.clock_hand = (self.clock_hand + 1) % self.frames                
+                
 
     def get_total_disk_reads(self):
         # TODO: Implement the method to get total disk reads
-        return -1
+        return self.disk_reads
 
     def get_total_disk_writes(self):
         # TODO: Implement the method to get total disk writes
-        return -1
+        return self.disk_writes
 
     def get_total_page_faults(self):
         # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
--- prac2/lrummu.py
+++ prac2/lrummu.py
@@ -2,33 +2,86 @@ from mmu import MMU
 
 class LruMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for LruMMU
-        pass
+        self.access_time = 0
+        self.page_table = {}    # {page_num: {'frame': frame_id, 'dirty': is_write}}
+        self.last_used = {}     # {page_num: last_access_time}
+        self.num_frames = frames # the total number of frames
+        self.debug = False       # for debug mode
+        self.free_frames = list(range(frames)) # the list of free frames
+        self.disk_reads = 0     # the total number of disk reads
+        self.disk_writes = 0   # the total number of disk writes
+        self.page_faults = 0  # the total number of page faults
 
     def set_debug(self):
-        # TODO: Implement the method to set debug mode
-        pass
+        self.debug = True
 
     def reset_debug(self):
-        # TODO: Implement the method to reset debug mode
-        pass
+        self.debug = False
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        pass
+        self.access_page(page_number, False)
 
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        pass
+        self.access_page(page_number, True)
 
     def get_total_disk_reads(self):
-        # TODO: Implement the method to get total disk reads
-        return -1
+        return self.disk_reads
 
     def get_total_disk_writes(self):
-        # TODO: Implement the method to get total disk writes
-        return -1
+        return self.disk_writes
 
     def get_total_page_faults(self):
-        # TODO: Implement the method to get total page faults
-        return -1
+        return self.page_faults
+
+    def lru_paging(self):
+        evict = min(self.last_used, key = self.last_used.get) # get lru page
+        record = self.page_table.pop(evict)  # remove the lru from page table
+        self.last_used.pop(evict) # remove the lru page from last used record
+
+        # if the page is dirty, write to disk
+        if record['dirty']:      
+          self.disk_writes += 1
+          if self.debug:
+            print(f"page = {evict}, evict from frame {record['frame']}, dirty, write to disk")
+        
+        self.free_frames.append(record['frame']) # add the frame id, so this frame is free
+
+    def access_page(self, page_num, is_write):
+        self.access_time += 1
+
+        # Hit
+        if page_num in self.page_table:
+            self.last_used[page_num] = self.access_time
+
+            if is_write:
+                self.page_table[page_num]['dirty'] = True
+            if self.debug:
+                if is_write:
+                    print(f"page = {page_num}, hit, write")
+                else:
+                    print(f"page = {page_num}, hit, read")
+            return 
+        
+        # Miss
+        self.page_faults += 1
+        if self.debug:
+            if is_write:
+                print(f"page = {page_num}, miss, write")
+            else:
+                print(f"page = {page_num}, miss, read")        
+        
+        # no free frame, implement LRU paging
+        if not self.free_frames:
+            self.lru_paging()
+        
+        # allocate frame for the page
+        frame_id = self.free_frames.pop()
+        self.page_table[page_num] = {'frame': frame_id, 'dirty': is_write}
+        self.last_used[page_num] = self.access_time
+        self.disk_reads += 1
+        if self.debug:
+            print(f"page = {page_num}, load into the free frame {frame_id}")
+        
+        
+ 
+
